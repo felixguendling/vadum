6db3f7b4afb9a76f6057abf4c60679325b6c3db9
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2018-10-14 21:45:37 +0200

    recusive clone

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <sstream>
 
 #include "boost/filesystem.hpp"
@@ -22,50 +23,84 @@ struct dep {
     return url_.substr(slash_pos + 1, dot_pos - slash_pos - 1);
   }
 
+  friend bool operator<(dep const& a, dep const& b) { return a.url_ < b.url_; }
+
+  friend bool operator==(dep const& a, dep const& b) {
+    return a.url_ == b.url_;
+  }
+
   std::string url_, ref_;
+  std::string required_by_;
 };
 
-std::vector<dep> read_deps() {
+std::vector<dep> read_deps(fs::path const& p) {
+  if (!fs::is_regular_file(p)) {
+    return {};
+  }
+
   std::vector<dep> deps;
 
-  auto const curr_dir = fs::current_path();
-  for (auto const& p : {curr_dir / ".pkg", curr_dir.parent_path() / ".pkg"}) {
-    if (!fs::is_regular_file(".pkg")) {
+  std::ifstream f{p.string().c_str()};
+  f.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+  std::string line, base, path, ref;
+  while (!f.eof() && f.peek() != EOF && std::getline(f, line)) {
+    if (line.empty()) {
       continue;
     }
-
-    std::ifstream f{p.string().c_str()};
-    f.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-
-    std::string line, base, path, ref;
-    while (!f.eof() && f.peek() != EOF && std::getline(f, line)) {
-      if (line.empty()) {
-        continue;
-      }
-      if (line[0] == '*') {
-        base = line.substr(1);
-      } else {
-        std::stringstream ss{line};
-        ss >> path >> ref;
-        deps.push_back(dep{base.empty() ? path : base + ":" + path, ref});
-      }
-    }
-
-    if (!deps.empty()) {
-      break;
+    if (line[0] == '*') {
+      base = line.substr(1);
+    } else {
+      std::stringstream ss{line};
+      ss >> path >> ref;
+      deps.push_back(
+          dep{base.empty() ? path : base + ":" + path, ref, p.string()});
     }
   }
 
   return deps;
 }
 
+std::vector<dep> read_deps() {
+  auto const curr_dir = fs::current_path();
+  for (auto const& p : {curr_dir / ".pkg", curr_dir.parent_path() / ".pkg"}) {
+    if (auto const deps = read_deps(p); !deps.empty()) {
+      return deps;
+    }
+  }
+  std::cout << "no dependencies found\n";
+  return {};
+}
+
+void clone_dep_recursive(std::set<dep>& visited, fs::path const& deps_root,
+                         dep const& d) {
+  auto const path = deps_root / d.name();
+  uLOG(utl::info) << "cloning " << d.url_ << " [" << d.ref_ << "]"
+                  << " to " << path;
+  git_clone(d.url_, path.generic_string(), d.ref_);
+  for (auto const& d : read_deps(path / ".pkg")) {
+    if (auto const it = visited.find(d); it == end(visited)) {
+      visited.insert(d);
+      clone_dep_recursive(visited, deps_root, d);
+    } else if (it->ref_ != d.ref_) {
+      std::cout << "dependency conflict:\n"
+                << "  " << it->required_by_ << " requires\n    " << it->ref_
+                << "\n"
+                << "  " << d.required_by_ << " requires\n    " << d.ref_
+                << "\n";
+      throw std::runtime_error("non-matching references");
+    }
+  }
+}
+
 int main(int argc, char** argv) {
   auto const deps_root = fs::path("deps");
   fs::remove_all(deps_root);
   fs::create_directories(deps_root);
+  std::set<dep> visited;
   for (auto const& d : read_deps()) {
-    auto const path = deps_root / d.name();
-    uLOG(utl::info) << "cloning " << d.url_ << " to " << path;
-    git_clone(d.url_, path.generic_string(), d.ref_);
+    if (visited.insert(d).second) {
+      clone_dep_recursive(visited, deps_root, d);
+    }
   }
 }
