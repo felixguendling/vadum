beb780a9f669ee8d4bd1627a6a4f95eb0f8d07b6
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2018-10-16 13:27:25 +0200

    update

--- src/main.cc
+++ src/main.cc
@@ -5,109 +5,153 @@
 
 #include "boost/filesystem.hpp"
 
+#include "utl/erase.h"
+#include "utl/get_or_create.h"
 #include "utl/logging.h"
 #include "utl/parser/util.h"
+#include "utl/to_vec.h"
 
 #include "pkg/git_clone.h"
 
 namespace fs = boost::filesystem;
 using namespace pkg;
 
-struct dep {
-  std::string name() const {
-    auto const slash_pos = url_.find_last_of('/');
-    auto const dot_pos = url_.find_last_of('.');
-    verify(slash_pos != std::string::npos, "no slash in url");
-    verify(dot_pos != std::string::npos, "no dot in url");
-    verify(slash_pos < dot_pos, "slash and to in wrong order");
-    return url_.substr(slash_pos + 1, dot_pos - slash_pos - 1);
+constexpr auto const PKG_FILE = ".pkg";
+constexpr auto const ROOT = ".";
+
+struct dependency_loader {
+  struct dep {
+    dep(std::string url, std::string ref)
+        : url_{std::move(url)}, ref_{std::move(ref)} {}
+
+    std::string name() const {
+      if (url_ == ROOT) {
+        return ROOT;
+      }
+
+      auto const slash_pos = url_.find_last_of('/');
+      auto const dot_pos = url_.find_last_of('.');
+      verify(slash_pos != std::string::npos, "no slash in url");
+      verify(dot_pos != std::string::npos, "no dot in url");
+      verify(slash_pos < dot_pos, "slash and to in wrong order");
+      return url_.substr(slash_pos + 1, dot_pos - slash_pos - 1);
+    }
+
+    friend bool operator<(dep const& a, dep const& b) {
+      return a.url_ < b.url_;
+    }
+
+    friend bool operator==(dep const& a, dep const& b) {
+      return a.url_ == b.url_;
+    }
+
+    std::string url_, ref_;
+    std::set<dep*> preds_;
+    std::set<dep*> succs_;
+  };
+
+  explicit dependency_loader(fs::path deps_root)
+      : deps_root_{std::move(deps_root)} {}
+
+  std::vector<std::pair<std::string, std::string>> read_deps(dep const* d) {
+    auto const p = (d->name() == ROOT) ? (fs::path{ROOT} / PKG_FILE)
+                                       : (deps_root_ / d->name() / PKG_FILE);
+
+    std::cout << "deps from " << p << "\n";
+    if (!fs::is_regular_file(p)) {
+      std::cout << "  " << p << " is not a regular file\n";
+      return {};
+    }
+
+    std::ifstream f{p.string().c_str()};
+    f.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    std::vector<std::pair<std::string, std::string>> deps;
+    std::string line, base, path, ref;
+    while (!f.eof() && f.peek() != EOF && std::getline(f, line)) {
+      if (line.empty()) {
+        continue;
+      }
+
+      if (line[0] == '*') {
+        base = line.substr(1);
+      } else {
+        std::stringstream ss{line};
+        ss >> path >> ref;
+        deps.emplace_back(base.empty() ? path : base + ":" + path, ref);
+      }
+    }
+
+    for (auto const& [url, ref] : deps) {
+      std::cout << "  " << url << " " << ref << "\n";
+    }
+
+    return deps;
   }
 
-  friend bool operator<(dep const& a, dep const& b) { return a.url_ < b.url_; }
-
-  friend bool operator==(dep const& a, dep const& b) {
-    return a.url_ == b.url_;
+  void retrieve(dep* pred) {
+    for (auto const& url_ref : read_deps(pred)) {
+      auto const url = url_ref.first;
+      auto const ref = url_ref.second;
+      auto succ = utl::get_or_create(deps_, url, [&]() {
+        auto next =
+            dep_mem_.emplace_back(std::make_unique<dep>(url, ref)).get();
+        git_clone(next->url_, (deps_root_ / next->name()).string(), next->ref_);
+        retrieve(next);
+        return next;
+      });
+      verify(succ->ref_ == ref, "non-matching ref");
+      succ->preds_.insert(pred);
+      pred->succs_.insert(succ);
+    }
   }
 
-  std::string url_, ref_;
-  std::string required_by_;
+  void retrieve(fs::path const& p) {
+    auto& d = dep_mem_.emplace_back(std::make_unique<dep>(ROOT, ROOT));
+    retrieve(deps_[ROOT] = d.get());
+  }
+
+  std::vector<dep*> sorted() {
+    auto sorted = std::vector<dep*>{};
+    auto all = utl::to_vec(dep_mem_, [](auto&& d) { return d.get(); });
+    while (true) {
+      auto const dependency_free = std::find_if(
+          begin(all), end(all), [](auto&& d) { return d->succs_.empty(); });
+      if (dependency_free == end(all)) {
+        return sorted;
+      }
+
+      std::cout << (*dependency_free)->name() << " has no dependencies\n";
+
+      if ((*dependency_free)->name() != ROOT) {
+        sorted.emplace_back(*dependency_free);
+      }
+      for (auto const& pred : (*dependency_free)->preds_) {
+        std::cout << "  removing " << (*dependency_free)->name() << " from "
+                  << pred->name() << "\n";
+        utl::erase(pred->succs_, (*dependency_free));
+      }
+      all.erase(dependency_free);
+    }
+  }
+
+  fs::path deps_root_;
+  std::map<std::string, dep*> deps_;
+  std::vector<std::unique_ptr<dep>> dep_mem_;
 };
-
-std::vector<dep> read_deps(fs::path const& p) {
-  if (!fs::is_regular_file(p)) {
-    return {};
-  }
-
-  std::vector<dep> deps;
-
-  std::ifstream f{p.string().c_str()};
-  f.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-
-  std::string line, base, path, ref;
-  while (!f.eof() && f.peek() != EOF && std::getline(f, line)) {
-    if (line.empty()) {
-      continue;
-    }
-    if (line[0] == '*') {
-      base = line.substr(1);
-    } else {
-      std::stringstream ss{line};
-      ss >> path >> ref;
-      deps.push_back(
-          dep{base.empty() ? path : base + ":" + path, ref, p.string()});
-    }
-  }
-
-  return deps;
-}
-
-std::vector<dep> read_deps() {
-  auto const curr_dir = fs::current_path();
-  for (auto const& p : {curr_dir / ".pkg", curr_dir.parent_path() / ".pkg"}) {
-    if (auto const deps = read_deps(p); !deps.empty()) {
-      return deps;
-    }
-  }
-  std::cout << "no dependencies found\n";
-  return {};
-}
-
-void clone_dep_recursive(std::set<dep>& visited, fs::path const& deps_root,
-                         dep const& d) {
-  auto const path = deps_root / d.name();
-  uLOG(utl::info) << "cloning " << d.url_ << " [" << d.ref_ << "]"
-                  << " to " << path;
-  git_clone(d.url_, path.generic_string(), d.ref_);
-  for (auto const& d : read_deps(path / ".pkg")) {
-    if (auto const it = visited.find(d); it == end(visited)) {
-      visited.insert(d);
-      clone_dep_recursive(visited, deps_root, d);
-    } else if (it->ref_ != d.ref_) {
-      std::cout << "dependency conflict:\n"
-                << "  " << it->required_by_ << " requires\n    " << it->ref_
-                << "\n"
-                << "  " << d.required_by_ << " requires\n    " << d.ref_
-                << "\n";
-      throw std::runtime_error("non-matching references");
-    }
-  }
-}
 
 int main(int argc, char** argv) {
   auto const deps_root = fs::path("deps");
   fs::remove_all(deps_root);
   fs::create_directories(deps_root);
-  std::set<dep> visited;
-  for (auto const& d : read_deps()) {
-    if (visited.insert(d).second) {
-      clone_dep_recursive(visited, deps_root, d);
-    }
-  }
+
+  dependency_loader l{deps_root};
+  l.retrieve(fs::path{"."});
 
   std::ofstream of{"deps/CMakeLists.txt"};
   of << "project(" + deps_root.string() << ")\n"
      << "cmake_minimum_required(VERSION 3.11)\n\n";
-  for (auto const& v : visited) {
-    of << "add_subdirectory(" << v.name() << " EXCLUDE_FROM_ALL)\n";
+  for (auto const& v : l.sorted()) {
+    of << "add_subdirectory(" << v->name() << " EXCLUDE_FROM_ALL)\n";
   }
 }
