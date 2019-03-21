007947d8e4bdf25b9fc03fc67e335e96da6a634f
Simon Gündling <sguendling@algo.informatik.tu-darmstadt.de>
2019-03-21 15:24:47 +0100

    remove bootstrap scripts

--- bootstrap.bat
+++
@@ -1,8 +0,0 @@
-echo [bootstrap.bat] fetch precompiled pkg
-git clone --bare --branch master --depth 1 git@git.motis-project.de:dl/pkg.git
-git --git-dir pkg.git show HEAD:pkg.exe > pkg.exe
-
-echo [bootstrap.bat] load local deps
-pkg.exe -l
-
-echo [bootstrap.bat] done.
