fa460c2a32e3daa7268f98bac95a70f2fb24d83d
Simon Gündling <sguendling@algo.informatik.tu-darmstadt.de>
2019-03-21 15:03:24 +0100

    work around add_subdirectory(.)

--- cmake/pkg.cmake
+++ cmake/pkg.cmake
@@ -1,9 +1,20 @@
-set(pkg-bin "${CMAKE_CURRENT_BINARY_DIR}/pkg")
+MACRO(SUBDIRLIST result curdir)
+FILE(GLOB children RELATIVE ${curdir} ${curdir}/*)
+SET(dirlist "")
+FOREACH(child ${children})
+  IF(IS_DIRECTORY ${curdir}/${child})
+    LIST(APPEND dirlist ${child})
+  ENDIF()
+ENDFOREACH()
+SET(${result} ${dirlist})
+ENDMACRO()
+
+set(pkg-bin "${CMAKE_CURRENT_BINARY_DIR}/dl/pkg")
 
 if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
-  set(pkg-url "https://git.motis-project.de/dl/pkg/-/jobs/45575/artifacts/raw/build-release/pkg")
+  set(pkg-url "https://git.motis-project.de/dl/pkg/-/jobs/45539/artifacts/raw/build-release/pkg")
 elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
-  set(pkg-url "https://git.motis-project.de/dl/pkg/-/jobs/45576/artifacts/raw/build-msvc-release/pkg.exe")
+  set(pkg-url "https://git.motis-project.de/dl/pkg/-/jobs/45540/artifacts/raw/build-msvc-release/pkg.exe")
 else()
   message(STATUS "Not downloading pkg tool. Using pkg from PATH.")
   set(pkg-bin "pkg")
@@ -26,3 +37,8 @@ execute_process(
   COMMAND ${pkg-bin} -l
   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 )
+
+SUBDIRLIST(SUBDIRS "${CMAKE_CURRENT_SOURCE_DIR}/deps")
+foreach(subdir ${SUBDIRS})
+    add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/deps/${subdir}" EXCLUDE_FROM_ALL)
+endforeach()
