085b88fdd234294d7f67fbc33c490953538136c4
Felix Gündling <felix.guendling@gmail.com>
2020-03-24 08:47:59 +0100

    cleanup, only create release for tag

--- example/src/main.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "repo1/repo1.h"
-#include "repo2/repo2.h"
-
-int main() {
-  std::cout << "random number: " << repo1::repo1() << " - " << repo2::repo2()
-            << "\n";
-}
